c6bf8b8af3d21c044b0de29b90fb477f9c23206f
Rachael-ED <rachaelwith2as@gmail.com>
2024-07-14 19:09:01 -0400

    Add files via upload

---
+++ AudioHelper.py
@@ -0,0 +1,50 @@
+# ==============================================================================
+# IMPORTS
+#
+import sys
+from PyQt5.Qt import *
+from PyQt5.QtCore import QObject, pyqtSignal
+import AudioHelperGUI as GuiMdl
+import AudioGen as AudGenMdl
+import logging
+
+# ==============================================================================
+# MAIN PROGRAM
+#
+
+logging.basicConfig(format="%(asctime)s %(levelname)s: %(message)s", level=logging.INFO)
+
+print(f"Hey User!  The program has started.")
+
+# --- Create GUI ---
+app = QApplication(sys.argv)
+main_win = GuiMdl.AudioHelperGUI()
+main_win.show()
+
+# --- Create AudioGen ---
+audio_gen_thread = QThread()
+audio_gen = AudGenMdl.AudioGen()
+audio_gen.enable()
+
+# --- Make Window & AudioGen Connections ---
+# It seems that these need to be done before we move the AudioGen to its own thread.
+main_win.sig_audio_gen_enable.connect(audio_gen.enable)
+
+main_win.sig_closing.connect(audio_gen.stop)                      # When user closes main window, stop audio generator
+audio_gen.finished.connect(audio_gen_thread.quit)                 # ... Once the generator is done, quit the thread
+audio_gen.finished.connect(audio_gen.deleteLater)                 # ...     and schedule the generator to be deleted
+
+# --- Move AudioGen to Its Own Thread ---
+audio_gen.moveToThread(audio_gen_thread)
+
+# --- Thread Connections ---
+# It seems that these need to be set up after AudioGen is Moved to the Thread
+audio_gen_thread.started.connect(audio_gen.run)                   # Start audio gen running when its thread is started
+audio_gen_thread.finished.connect(audio_gen_thread.deleteLater)   # Once the thread is done, schedule it to be deleted
+
+# --- Start the Threads and the Event Loop ---
+audio_gen_thread.start()
+app.exec()
+
+# --- Finished ---
+print("DONE")
