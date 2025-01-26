c23ea7a65da9b949a88af0bb14e5eaa073fde579
Fahthar <steve.jennifer@sympatico.ca>
2025-01-26 00:10:59 -0500

    a) Added averaging waveform to smooth out live waveform b) Changes to plot: - Added legend - Reduce font of axis labels - limit freq to 50Hz-20kHz

--- AudioHelperGUI.py
+++ AudioHelperGUI.py
@@ -34,6 +34,9 @@ C_SPEC_MAX_DB = 80
 C_SPEC_MIN_DB = -80
 C_SPEC_GRID_DB = 10
 
+C_SPEC_MAX_FREQ = 20000    # [Hz]
+C_SPEC_MIN_FREQ = 50       # [Hz]
+
 # ==============================================================================
 # CLASS: MAIN WINDOW
 #
@@ -84,12 +87,26 @@ class AudioHelperGUI(QMainWindow, Ui_ui_AudioHelperGUI):
         self.plt_ax.semilogx()
         self.plt_ax.set_xlabel('Frequency [Hz]')
         self.plt_ax.set_ylabel('Amplitude [dB]')
-        self.plt_line_meas_freq = np.linspace(50, 50000, 16384)
-        self.plt_line_meas_ampl = np.random.randint(0, 10, 16384)
         self.plt_ax.set_ylim(C_SPEC_MIN_DB, C_SPEC_MAX_DB)
+        self.plt_ax.set_xlim(C_SPEC_MIN_FREQ, C_SPEC_MAX_FREQ)
         self.plt_ax.set_yticks(np.arange(C_SPEC_MIN_DB, C_SPEC_MAX_DB, C_SPEC_GRID_DB))
-        plt_refs = self.plt_ax.plot(self.plt_line_meas_freq, self.plt_line_meas_ampl)
+        self.plt_ax.xaxis.set_tick_params(labelsize="small")
+        self.plt_ax.yaxis.set_tick_params(labelsize="small")
+
+        # Create Plot for Latest Amplitude Measurement
+        self.plt_line_meas_freq = np.linspace(C_SPEC_MIN_FREQ, C_SPEC_MAX_FREQ, 16384)
+        self.plt_line_meas_ampl = np.array([0] * 16384)
+        plt_refs = self.plt_ax.plot(self.plt_line_meas_freq, self.plt_line_meas_ampl, label = 'Live')
         self.plt_line_meas = plt_refs[0]
+
+        # Create Plot for Average Amplitude
+        self.plt_line_avg_freq = np.linspace(C_SPEC_MIN_FREQ, C_SPEC_MAX_FREQ, 16384)
+        self.plt_line_avg_ampl = np.array([0] * 16384)
+        plt_refs = self.plt_ax.plot(self.plt_line_avg_freq, self.plt_line_avg_ampl, label = "Average")
+        self.plt_line_avg = plt_refs[0]
+
+        # Add Legend
+        self.plt_ax.legend(fontsize="small")
 
         # Configure AudioGen Widgets
         self.txt_aud_gen_freq1.setValidator(QDoubleValidator())
@@ -271,35 +288,33 @@ class AudioHelperGUI(QMainWindow, Ui_ui_AudioHelperGUI):
             self.btn_aud_ana_enable.setText("Freeze")
 
     def update_plot(self, buf_id):
-        """
-        self.buf_id_list.append(buf_id)
-        if (len(self.buf_id_list) < 4):
-            return
-        #elif (len(self.buf_id_list) > 8):
-        #    pass
-        elif (random.random() < 0.1):
-            return
-        buf_id = self.buf_id_list.pop(0)
-        """
-
-        buf = self.buf_man.free(buf_id)
-        self.plt_line_meas_freq = buf[0]
-        self.plt_line_meas_ampl = buf[1]
+        # Retrieve Buffer to Plot
+        [name, freq_list, ampl_list] = self.buf_man.free(buf_id)
 
         # Remove DC element
-        self.plt_line_meas_freq = np.delete(self.plt_line_meas_freq,0)
-        self.plt_line_meas_ampl = np.delete(self.plt_line_meas_ampl,0)
+        freq_list = np.delete(freq_list,0)
+        ampl_list = np.delete(ampl_list,0)
 
-        self.plt_line_meas_ampl = np.clip(self.plt_line_meas_ampl, 1e-6, None)
-        self.plt_line_meas_ampl = 20 * np.log10(self.plt_line_meas_ampl)
-        self.plt_line_meas_ampl = np.clip(self.plt_line_meas_ampl, C_SPEC_MIN_DB, C_SPEC_MAX_DB)
+        # Tranlsate to dB
+        ampl_list = np.clip(ampl_list, 1e-6, None)      # np.log10() won't like 0s
+        ampl_list = 20 * np.log10(ampl_list)                         # Translate to dB
+        ampl_list = np.clip(ampl_list, C_SPEC_MIN_DB, C_SPEC_MAX_DB) # Limit to plot range
 
-        self.plt_ax.set_xlim(self.plt_line_meas_freq[0], self.plt_line_meas_freq[-1])
-        #self.plt_ax.set_ylim(np.min(self.plt_line_meas_ampl), np.max(self.plt_line_meas_ampl))
+        # Update Measurement Plot
+        if (name == "meas"):
+            self.plt_line_meas_freq = freq_list
+            self.plt_line_meas_ampl = ampl_list
+            self.plt_line_meas.set_data(self.plt_line_meas_freq, self.plt_line_meas_ampl)
+            self.plt_line_meas.figure.canvas.draw()
 
-        self.plt_line_meas.set_data(self.plt_line_meas_freq, self.plt_line_meas_ampl)
-        self.plt_line_meas.figure.canvas.draw()
+        elif (name == "avg"):
+            self.plt_line_avg_freq = freq_list
+            self.plt_line_avg_ampl = ampl_list
+            self.plt_line_avg.set_data(self.plt_line_avg_freq, self.plt_line_avg_ampl)
+            self.plt_line_avg.figure.canvas.draw()
 
+        else:
+            logging.info(f"ERROR: Invalid plot line to update ({name})")
 
 # ==============================================================================
 # MODULE TESTBENCH
