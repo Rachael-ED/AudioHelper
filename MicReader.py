04bcb7676c8e32f4eb1a6a330f08e46e40f43618
Rachael-ED <rachaelwith2as@gmail.com>
2025-02-24 20:12:56 -0500

    Change input index and send updated input index to Guido, both via Buff Man

--- MicReader.py
+++ MicReader.py
@@ -52,8 +52,6 @@ class MicReader(QObject):
         self.channels = channels
         self.rate = rate
         self.framesPerBuffer = 16384     # i.e. 2^14
-        #self.inputIndex = 1     # For Rachael's MacBook Pro WITH headphones, computer mic = 2, else mic = 1
-        #self.inputIndex = 2     # For Fahthar's MacBook Pro MacBook mic = 2
         self._reopen_stream = False
 
         # instantiate PyAudio
@@ -69,7 +67,10 @@ class MicReader(QObject):
 
     def msgHandler(self, buf_id):
         [msg_type, snd_name, msg_data] = self.buf_man.msgReceive(buf_id)
-        logging.info(f"ERROR: {self.name} received unsupported {msg_type} message from {snd_name} : {msg_data}")
+        if msg_type == "change_input":
+            self.changeInputIndex(msg_data)
+        else:
+            logging.info(f"ERROR: {self.name} received unsupported {msg_type} message from {snd_name} : {msg_data}")
 
     def enable(self, audio_on=True):
         self._audio_on = audio_on
@@ -120,6 +121,7 @@ class MicReader(QObject):
         self.inputIndex = newInputIndex
         self._reopen_stream = True
         logging.info(f"input index: {newInputIndex}")
+        self.buf_man.msgSend("Guido", "default_input", self.inputIndex)
 
 # ==============================================================================
 # MODULE TESTBENCH
